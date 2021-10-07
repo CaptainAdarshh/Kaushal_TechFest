78731882456bc933e4a011bdb09f6cc8a47fa7df
Saurav017 <sauravabde@gmail.com>
2021-10-07 08:38:18 +0530

    Modal Section Added

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.nav-menu');
+const navLinks = document.querySelector('.nav-links')
 
 menu.addEventListener('click', function() {
     menu.classList.toggle('is-active');
     console.log("Button is active");
     menuLinks.classList.toggle('active');
+    navLinks.classList.toggle("is-open");
 })
 
 
@@ -32,3 +34,35 @@ const countdown = () => {
 }
 
 countdown()
+
+
+// Modal
+
+// Get the modal
+let modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+let btn = document.querySelectorAll(".know-more");
+
+// Get the <span> element that closes the modal
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+for (i of btn) {
+    i.addEventListener('click', function () {
+        console.log("Clicked");
+        modal.style.display = "block";
+    })
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
